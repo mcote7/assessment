d4ea28cabe893a922c455cec0538d123b18d84b9
Michael Cote <michaelcote7@outlook.com>
2020-08-18 22:31:20 -0700

    .

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -44,18 +44,18 @@ const Student = ({result, inputId, tagSearchQuery}) => {
     input.value = '';
   };
 
-  const handleDelete = (e) => {
-    // const myTags = [...tags];
-    // const innerText = e.currentTarget.parentNode.textContent;
-    // console.log("myTags:", myTags);
-    // console.log("innerText:",innerText);
-    // if(myTags.toString().includes(innerText)) {
-    //   let newTags = [];
-    //   newTags = myTags.toString().filter(t=>t !== myTags.includes(innerText))
-    //   console.log("result:", newTags);
-    // } 
-    // e.currentTarget.parentNode.remove();
-  };
+  // const handleDelete = (e) => {
+  //   const myTags = [...tags];
+  //   const innerText = e.currentTarget.parentNode.textContent;
+  //   console.log("myTags:", myTags);
+  //   console.log("innerText:",innerText);
+  //   if(myTags.toString().includes(innerText)) {
+  //     let newTags = [];
+  //     newTags = myTags.toString().filter(t=>t !== myTags.includes(innerText))
+  //     console.log("result:", newTags);
+  //   } 
+  //   e.currentTarget.parentNode.remove();
+  // };
 
   if(searching && !match) return null;
   if((searching && match)||(!searching && !match)) {
@@ -82,9 +82,9 @@ const Student = ({result, inputId, tagSearchQuery}) => {
           {details && result.grades.map((g, idx) =>
           <p key={idx}>{`Test: ${g}%`}</p>)}
 
-          {details ? tags.map((tag, i)=> <p className="myBadge mt-2 mr-2 px-2 py-1" key={i}>{tag}&nbsp;&nbsp;
+          {details ? tags.map((tag, i)=> <p className="myBadge mt-2 mr-2 px-2 py-1" key={i}>{tag}</p>):''}
           {/* check delete */}
-          <span onClick={(e) => handleDelete(e)} className="deleteBtn"><i className="fa fa-plus tagIcon" aria-hidden="true"></i></span></p>):''}
+          {/* &nbsp;&nbsp;<span onClick={(e) => handleDelete(e)} className="deleteBtn"><i className="fa fa-plus tagIcon" aria-hidden="true"></i></span> */}
 
           {details ?
           <form onSubmit={(e)=> handleTags(e)}>
